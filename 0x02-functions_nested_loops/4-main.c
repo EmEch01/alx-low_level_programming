340211f6075186bd787a3f88186a36953cf987cd
Emch01 <rhamzee13@gmail.com>
2023-06-21 12:46:23 -0700

    4-isalpha

--- 0x02-functions_nested_loops/4-main.c
+++ 0x02-functions_nested_loops/4-main.c
@@ -9,10 +9,10 @@ int main(void)
 {
     int r;
 
-    r = _isalpha('H');
-    _putchar(r + '0');
+    r =  _isalpha('H');
+     _putchar(r + '0');
     r = _isalpha('o');
-    _putchar(r + '0');
+     _putchar(r + '0');
     r = _isalpha(108);
     _putchar(r + '0');
     r = _isalpha(';');
